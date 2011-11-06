f62171cc8bf4a70b70fc3fb1d2c164ca544fc608
Georgy Bolyuba <bolyuba@gmail.com>
2011-11-06 23:31:00 +0100

    initial commit for ex2, ex3, ex4

--- mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/gradientDescentMulti.m
@@ -17,15 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+	delta = (((theta' * X')') - y);
+	theta = (theta' - ((delta' * X) / m * alpha))';
 
     % ============================================================
 
